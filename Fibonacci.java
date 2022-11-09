10b4c27e6491236c36514b4122752b963e879736
Chaitanya Bandiwdekar <67865461+ChaitanyaBandiwdekar@users.noreply.github.com>
2022-11-09 11:38:39 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,12 @@
-class FibonacciExample1 {  
-    public static void main(String args[]) {    
-        int n1=0, n2=1, n3, i, count=10;    
-        System.out.print(n1 + " " + n2);
-            
-        for(i=2; i<count; ++i) {    
-            n3=n1+n2;    
-            System.out.print(" " + n3);    
-            n1 = n2;    
-            n2 = n3;    
-        }    
+public class Fibonacci {
+    public static int fibonacci(int num) {
+        if(num= 0) return 0;
+        if(num= 1 || num=-2) return 1;
+        return fibonacci (num-1) + fibonacci (num-2);
     }
-}  
+    public static void main(String[] args) {
+        int n = 5;
+        int fib = fibonacci(n);
+        System.out.println(fib);
+    }
+}
